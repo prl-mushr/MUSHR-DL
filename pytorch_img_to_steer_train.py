ae3c3b045acdd0b203ef4cdddb4d43076b3ee392
naughtyStark <sidharthtalia@ymail.com>
2020-06-19 19:27:21 +0530

    Update pytorch_img_to_steer_train.py

--- pytorch_img_to_steer_train.py
+++ pytorch_img_to_steer_train.py
@@ -2,35 +2,38 @@ from pytorch_high_level import *
 from pytorch_model import Net
 torch.backends.cudnn.benchmark = True
 torch.backends.cudnn.enabled = True
+import argparse
+
+parser = argparse.ArgumentParser(description='training')
+parser.add_argument('--dataset_name', type=str, default="4", help='suffix for the dataset name')
+args = parser.parse_args()
 
 HEIGHT = 240
 WIDTH = 320
 CHANNELS = 1
-
+model_name = 'steering.h5'
 
 net = Net(HEIGHT,WIDTH,CHANNELS).to(device)
-# optimizer = optim.Adam(net.parameters(),lr = 0.001)
 # loss_function = nn.MSELoss()
-# checkpoint = torch.load('steering.h5')
+# checkpoint = torch.load(model_name) # uncomment this to train an existing model
 # net = checkpoint['net']
 train_log = []
-for i in range(4):
-    torch.cuda.empty_cache()
-    train_data = np.load('MUSHR_320x240_shuffled_steering_{}.npy'.format(str(3)),allow_pickle=True)
-    X = torch.Tensor([i[0] for i in train_data]).view(-1,CHANNELS,HEIGHT,WIDTH)
-    Y = torch.Tensor([i[1] for i in train_data])
-    del train_data
-    train_log = fit(net,X,Y,train_log,optimizer='adam',loss_function='mean_square',validation_set=0.1,BATCH_SIZE=8,EPOCHS=10)
-    state = {'net':net}
-    torch.save(state,'steering.h5')
-    loss_function = None
-    optimizer = None
-    X = None
-    Y = None
-    del X
-    del Y
-    del loss_function
-    del optimizer
+torch.cuda.empty_cache()
+train_data = np.load('MUSHR_320x240_shuffled_Steering_{}.npy'.format(args.dataset_name),allow_pickle=True)
+X = torch.Tensor([i[0] for i in train_data]).view(-1,CHANNELS,HEIGHT,WIDTH)
+Y = torch.Tensor([i[1] for i in train_data])
+del train_data
+train_log = fit(net,X,Y,train_log,optimizer='adam',loss_function='mean_square',validation_set=0.1,BATCH_SIZE=8,EPOCHS=1)
+state = {'net':net}
+torch.save(state,model_name)
+loss_function = None
+optimizer = None
+X = None
+Y = None
+del X
+del Y
+del loss_function
+del optimizer
 
 a = time.localtime(time.time())
 log_file = 'steering_log_{}_{}_{}_{}_{}.npy'.format(a.tm_year,a.tm_mon,a.tm_mday,a.tm_hour,a.tm_min)
