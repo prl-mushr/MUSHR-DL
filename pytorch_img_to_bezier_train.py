9bbd7769e704104589950fbe3bc296fd926a138e
naughtyStark <sidharthtalia@ymail.com>
2020-06-19 19:24:49 +0530

    Update pytorch_img_to_bezier_train.py

--- pytorch_img_to_bezier_train.py
+++ pytorch_img_to_bezier_train.py
@@ -2,9 +2,11 @@ from pytorch_high_level import *
 from pytorch_model import Bezier
 torch.backends.cudnn.benchmark = True
 torch.backends.cudnn.enabled = True
+import argparse
 
 parser = argparse.ArgumentParser(description='IL')
 parser.add_argument('--dataset_name', type=str, default="4", help='suffix for the dataset name')
+args = parser.parse_args()
 
 
 HEIGHT = 240
@@ -13,35 +15,30 @@ CHANNELS = 1
 
 
 net = Bezier(HEIGHT,WIDTH,CHANNELS).to(device)
-# optimizer = optim.Adam(net.parameters(),lr = 0.001)
-# loss_function = nn.MSELoss()
-model_name = 'bezier_1.5.h5'
-# checkpoint = torch.load(model_name)
+model_name = 'bezier.h5'
+# checkpoint = torch.load(model_name) #uncomment this and the line below to train an existing model
 # net = checkpoint['net']
 train_log = []
-for _ in range(5):
-    for i in range(4):
-        torch.cuda.empty_cache()
-        train_data = np.load('MUSHR_320x240_shuffled_Bezier{}.npy'.format(str(i)),allow_pickle=True)
-        X = torch.Tensor([i[0] for i in train_data]).view(-1,CHANNELS,HEIGHT,WIDTH)
-        Y = torch.Tensor([i[1] for i in train_data])
-        del train_data
-        print(i*_)
-        train_log = fit(net,X,Y,train_log,optimizer='adam',loss_function='mean_square',validation_set=0.1,BATCH_SIZE=8,EPOCHS=2)
-        state = {'net':net}
-        torch.save(state,model_name)
-        loss_function = None
-        optimizer = None
-        train_data = None
-        X = None
-        Y = None
-        del train_data
-        del X
-        del Y
-        del loss_function
-        del optimizer
+
+torch.cuda.empty_cache()
+train_data = np.load('MUSHR_320x240_shuffled_Bezier_{}.npy'.format(args.dataset_name),allow_pickle=True)
+X = torch.Tensor([i[0] for i in train_data]).view(-1,CHANNELS,HEIGHT,WIDTH)
+Y = torch.Tensor([i[1] for i in train_data])
+del train_data
+train_log = fit(net,X,Y,train_log,optimizer='adam',loss_function='mean_square',validation_set=0.1,BATCH_SIZE=8,EPOCHS=1)
+state = {'net':net}
+torch.save(state,model_name)
+loss_function = None
+optimizer = None
+train_data = None
+X = None
+Y = None
+del train_data
+del X
+del Y
+del loss_function
+del optimizer
 
 a = time.localtime(time.time())
 log_file = 'bezier_log_{}_{}_{}_{}_{}.npy'.format(a.tm_year,a.tm_mon,a.tm_mday,a.tm_hour,a.tm_min)
 np.save(log_file,np.array(train_log))
-# TODO: add a snapshot step to autosave model.
