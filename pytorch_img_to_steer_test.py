2f70cba8e4470f292dd67ec751a5827dcb48a889
naughtyStark <sidharthtalia@ymail.com>
2020-06-19 19:26:47 +0530

    Update pytorch_img_to_steer_test.py

--- pytorch_img_to_steer_test.py
+++ pytorch_img_to_steer_test.py
@@ -1,47 +1,40 @@
 from pytorch_high_level import *
-# from pytorch_model import Net
+import argparse
 
+parser = argparse.ArgumentParser(description='testing')
+parser.add_argument('--dataset_name', type=str, default="4", help='suffix for the dataset name')
+args = parser.parse_args()
 
 HEIGHT = 240
 WIDTH = 320
 CHANNELS = 1
+model_name = 'steering.h5'
 
-
-# net = Net(HEIGHT,WIDTH,CHANNELS).to(device)
-# optimizer = optim.Adam(net.parameters(),lr = 0.001)
-# loss_function = nn.MSELoss()
-checkpoint = torch.load('steering.h5')
+checkpoint = torch.load(model_name)
 model = checkpoint['net']
 model = model.to(device)
-# train_log = []
-for i in range(1):
-    # optimizer = optim.Adam(net.parameters(),lr = 0.001)
-    # loss_function = nn.MSELoss()
-    # torch.cuda.empty_cache()
-    train_data = np.load('MUSHR_320x240_shuffled_steering_{}.npy'.format(str(i)),allow_pickle=True)
-    X = ([i[0] for i in train_data])
-    Y = ([i[1] for i in train_data])
-    with torch.no_grad():
-        for j in range(0,len(X),10):
-            img = np.array(X[j],dtype=np.float32)
-            time.sleep(0.001)
-            img = torch.Tensor(img).view(-1,1,240,320).to(device)
-            # print(img.dtype)
-            now = time.time()
-            output = model(img)[0]
-            delta = time.time()-now
-            print(delta*1000)
-            output = float(output[0] - output[1])
-            GT = Y[j]
-            GT = GT[0] - GT[1]
-            error = np.fabs(GT-output)
-            print(error)
-            # cv2.imshow('window',X[j])
-            # cv2.waitKey(1)
-            # time.sleep(1)
-    train_data = None
-    X = None
-    Y = None
-    del train_data
-    del X
-    del Y
+
+train_data = np.load('MUSHR_320x240_shuffled_Steering_{}.npy'.format(args.dataset_name),allow_pickle=True)
+X = ([i[0] for i in train_data])
+Y = ([i[1] for i in train_data])
+with torch.no_grad():
+    for j in range(0,len(X),10):
+        img = np.array(X[j],dtype=np.float32)
+        time.sleep(0.001)
+        img = torch.Tensor(img).view(-1,1,240,320).to(device)
+        # print(img.dtype)
+        now = time.time()
+        output = model(img)[0]
+        delta = time.time()-now
+        print(delta*1000)
+        output = float(output[0] - output[1])
+        GT = Y[j]
+        GT = GT[0] - GT[1]
+        error = np.fabs(GT-output)
+        print(error)
+train_data = None
+X = None
+Y = None
+del train_data
+del X
+del Y
