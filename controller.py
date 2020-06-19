76953f455383caec43c2d9ab1a92f63be8e4a374
naughtyStark <sidharthtalia@ymail.com>
2020-06-19 19:13:57 +0530

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,4 +1,3 @@
-import serial
 import time
 import traceback
 import numpy as np
@@ -12,54 +11,6 @@ def check_range(x,lim):
         return -lim
     return x
 
-# this is if you are using the hacky arduino+TxRx based controller (Script included)
-class remote_control():
-    def __init__(self):
-        self.st = 0.0
-        self.th = 0.0
-        self.autonomous = False
-        self.ser =  serial.Serial('COM4', 250000, timeout=0) # put in your controller's com port number here. 
-        if self.ser.isOpen():
-            self.ser.close()
-        self.ser.open()
-
-    def read_loop(self):
-        while 1:
-            try:
-                while(self.ser.inWaiting()<10):
-                    time.sleep(0.01)
-
-                buf = np.frombuffer(self.ser.read(10),dtype='int16')
-
-                if len(buf)==5:
-                    steering = buf[0]#ord(self.ser.read())<<8|ord(self.ser.read())
-                    throttle = buf[1]#ord(self.ser.read())<<8|ord(self.ser.read())
-                    right_stick = buf[2]#ord(self.ser.read())<<8|ord(self.ser.read())
-                    A = buf[3]#ord(self.ser.read())<<8|ord(self.ser.read())
-                    B = buf[4]#ord(self.ser.read())<<8|ord(self.ser.read())
-                    while self.ser.inWaiting():
-                        self.ser.read()
-                    self.st = (steering - 1500)*0.002
-                    self.th = (throttle - 1500)*0.0018
-                    AxisRx = (right_stick-1500)*0.002 
-                    #prevent steering and throttle from being outside the usable range.
-                    if(B>1500):
-                        self.autonomous = True
-                    else:
-                        self.autonomous = False 
-                    self.st = check_range(self.st,1.0)
-
-                    self.th = check_range(self.th,1.0)
-                    # print(steering,throttle)
-            except KeyboardInterrupt:
-                exit()
-            except Exception as e:
-                # pass
-                print(traceback.format_exc())
-            except:
-                pass
-
-# This one is used by default
 class keyboard_control():
     def __init__(self):
         self.st = 0.0
@@ -72,7 +23,6 @@ class keyboard_control():
             try:
                 time.sleep(0.01)
                 self.st,self.th = get_mouse_pos()
-                print(key_press())
                 if(key_press() == ['A']):
                     self.autonomous = True
                 if(key_press() == ['M']):
